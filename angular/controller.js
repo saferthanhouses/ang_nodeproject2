9983dd3f99a3bceb1beeb5f21c279d632e084a8e
saferthanhouses <joey_oliver@hotmail.com>
2015-08-15 16:49:36 +0100

    added getPost controller functionality

--- angular/controller.js
+++ angular/controller.js
@@ -1,25 +1,32 @@
 
 myApp.controller('myController', function($scope, myFactory){
 
-	// sanity check;
-	$scope.sanity = "";
-
-	// get the posts
-	getPosts()
+	// Logic for getting the new posts
+	getPosts() // on load
 	
-	// new post box initial text & input
-	$scope.newPost = "Type something here..."
-
-	$scope.submitPost = function() { }
-
 	function getPosts() {  
 		myFactory.getPosts()
 			.then(function(response){ assignPosts(response.data) });
 	}
-
 	function assignPosts(data)
 	{ 
-		$scope.myPosts =data
+		$scope.myPosts = data
+	}	
+
+	// new post box initial text & input
+	$scope.post = "Type something here..."
+
+	$scope.submitPost = function() { 
+		$scope.newPost = {
+		"title": $scope.newPostTitle,
+		"entry": $scope.newPostEntry
+		};
+		myFactory.uploadPost($scope.newPost)
+			.then(function(response){ finishUploadPost() });
+	};
+
+	function finishUploadPost() {
+		$scope.myPosts.push($scope.new_post);
 	}
 			// .then(function($scope.myPosts),
 			// 	   function(console.log("error"))
--- angular/factory.js
+++ angular/factory.js
@@ -1,12 +1,17 @@
 
 myApp.factory('myFactory', ['$http', function($http) {
 	// $http factory data
+	var urlBase = 'http://localhost:8080';
 	var factory = {};
 	factory.data=null;
 
 	factory.getPosts = function() {
-		return $http.get('http://localhost:8080');
+		return $http.get(urlBase);
 		// .then(function(response) { console.log(response.data); factory.data = response.data })
+	}
+
+	factory.uploadPost = function(post) {
+		return $http.post(urlBase, post);
 	}
 
 	return factory;  
